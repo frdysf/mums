166c9b3821683a3fce7c7f13e09cd584da96e52b
Farida Yusuf <f.yusuf@qmul.ac.uk>
2024-11-15 22:57:52 +0000

    First successful pass through full dataset. Data unverified
    Add str2midi via src/utils.py

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import itertools as it
+
+def str2midi(note_string):
+  """
+  SOURCE: https://pythonhosted.org/audiolazy/_modules/audiolazy/lazy_midi.html#str2midi
+  Given a note string name (e.g. "Bb4"), returns its MIDI pitch number.
+  """
+  MIDI_A4 = 69
+  data = note_string.strip().lower()
+  name2delta = {"c": -9, "d": -7, "e": -5, "f": -4, "g": -2, "a": 0, "b": 2}
+  accident2delta = {"b": -1, "#": 1, "x": 2}
+  accidents = list(it.takewhile(lambda el: el in accident2delta, data[1:]))
+  octave_delta = int(data[len(accidents) + 1:]) - 4
+  return (MIDI_A4 +
+          name2delta[data[0]] + # Name
+          sum(accident2delta[ac] for ac in accidents) + # Accident
+          12 * octave_delta # Octave
+          )
