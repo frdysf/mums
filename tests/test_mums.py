6d6a8690aaef3b94f28193ca5523b65dcdb4ed23
Farida Yusuf <f.yusuf@qmul.ac.uk>
2024-11-16 23:44:37 +0000

    Add comments

---
+++ tests/test_mums.py
@@ -0,0 +1,14 @@
+import pytest
+from ..src.mums import MUMSDataset
+
+# TODO: define class and individual test functions
+#       Aim to start and FINISH on Monday!
+#       Prioritise if needed!
+
+# generate fixture
+# test field values in json_data
+# test include_dirs
+# test blacklist
+# test __len__
+# test __getitem__
+# test LabelEncoder
